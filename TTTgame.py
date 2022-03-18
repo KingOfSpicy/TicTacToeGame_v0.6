859e2835bdb4cac07813fd8f805f3e5c4cbdb2cf
KingOfSpicy <sangowosan@gmail.com>
2022-03-18 19:31:22 +0200

    build the templete.

---
+++ TTTgame.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+
+ttt = Flask(__name__)
+
+@ttt.route("/" ,methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        return render_template('game.html')
+
+    return render_template('home.html')
+    
+    
+
+@ttt.route("/game")
+def game():
+    return render_template('game.html')
+    
+
+@ttt.route("/stats")
+def stats():
+    n = 0
+    return render_template('stats.html', n = n)
+
+
+if __name__ == "__main__":
+    ttt.run()
